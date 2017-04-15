5ddd8c99b9936ca3afc7190618360d4c996e77fe
jshenaop@gmail.com <jshenaop@gmail.com>
2017-04-15 16:49:26 -0500

    First commit.

---
+++ soleka.py
@@ -0,0 +1,14 @@
+import flask import Flask
+
+
+DEBUG = True
+HOST = '0.0.0.0'
+PORT = 8000
+
+app = Flask(__name__)
+
+
+
+
+if __ name__ == '__main__':
+    app.run(debug=DEBUG, host=HOST, port=PORT)
