290dd715e2901d1ecb5c269f4ec817828f90f1de
jshenao@gmail.com <jshenaop@gmail.com>
2017-04-26 16:12:27 -0500

    CORS Added.

--- soleka.py
+++ soleka.py
@@ -2,19 +2,21 @@
 
 from flask import Flask
 from flask_restful.utils import cors
+from flask_cors import CORS, cross_origin
 
 import config
 import models
 from resources_v1.predictions import predictions_api_v1
+from templates.templates import home
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(predictions_api_v1, url_prefix='/api/v1')
 
 @app.route('/')
 def index():
-    return """<h1>Text predictor for Voice of the Citizen</h1>
-              <p>Instructions: make a POST request to /api/v1/prediction sending {"text": "some text"} """
+    return home
 
 if __name__ == '__main__':
     models.initilize()
