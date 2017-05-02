079e684e7ee01246dc87512645a6e95d9ad406ed
jshenao@gmail.com <jshenaop@gmail.com>
2017-05-02 11:03:52 -0500

    More models ADDED.

---
+++ analytics/GARAGE/test.py
@@ -0,0 +1,18 @@
+
+from textblob.classifiers import NaiveBayesClassifier
+
+train = [
+    ('I love this sandwich.', 'pos'),
+    ('This is an amazing place!', 'pos'),
+    ('I feel very good about these beers.', 'pos'),
+    ('This is my best work.', 'pos'),
+    ("What an awesome view", 'pos'),
+    ('I do not like this restaurant', 'neg'),
+    ('I am tired of this stuff.', 'neg'),
+    ("I can't deal with this", 'neg'),
+    ('He is my sworn enemy!', 'neg'),
+    ('My boss is horrible.', 'neg')
+]
+
+cl = NaiveBayesClassifier(train)
+print(cl.classify("Their burgers are amazing"))
